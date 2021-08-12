a32b6d93db2913970cc6c5d70480d2ba37d01810
Matisz Orsolya <orsolya.matisz@frontendart.com>
2021-08-12 15:30:47 +0200

    Complexity testing

--- src/analyzer-test/ComplexityTest.java
+++ src/analyzer-test/ComplexityTest.java
@@ -1,43 +1,84 @@
-public class CodeDuplication {
-    private static final int WINDOW_WIDTH = 410;
-    private static final int WINDOW_HEIGHT = 600;
-    private static final int BUTTON_WIDTH = 80;
-    private static final int BUTTON_HEIGHT = 70;
-    private static final int MARGIN_X = 20;
-    private static final int MARGIN_Y = 60;
+public class ComplexityTest {
 
-    private JFrame window; // Main window
-    private JComboBox<String> comboCalcType, comboTheme;
-    private JTextField inText; // Input
-    private JButton btnC, btnBack, btnMod, btnDiv, btnMul, btnSub, btnAdd,
-            btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
-            btnPoint, btnEqual, btnRoot, btnPower, btnLog;
+    public complexityTest(const num, int val) {
+		const val2;
+        for(int i=1;i<=10000;i++){
+            System.out.println(i);
+            if(val2 == 15000){
+                System.out.println("val2 is 15000");
+            } else {
+                return num;
+            }
+            while (num > 1000) {
+                System.out.println("num is greater than 1000");
+                num = 0;
+            }
+        }
+    }
 
-    private char opt = ' '; // Save the operator
-    private boolean go = true; // For calculate with Opt != (=)
-    private boolean addWrite = true; // Connect numbers in display
-    private double val = 0; // Save the value typed for calculation
+    public void populateProducts() {
 
-
-    public double calc(double x, String input, char opt) {
-        inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
-        double y = Double.parseDouble(input);
-        switch (opt) {
-            case '+':
-                return x + y;
-            case '-':
-                return x - y;
-            case '*':
-                return x * y;
-            case '/':
-                return x / y;
-            case '%':
-                return x % y;
-            case '^':
-                return Math.pow(x, y);
-            default:
-                inText.setFont(inText.getFont().deriveFont(Font.PLAIN));
-                return y;
+        int dummyArraySize = 1;
+        while (dummyArraySize == 1) {
+            System.out.println("dummyArraySize is 1");
+        }
+        for (int loop = 0; loop < 10; loop++) {
+            if(loop%2 == 0) {
+                for(int i = 15000; i >= 1500; i++) {
+                    loop++;
+                    System.out.println("sadasda");
+                }
+                createObjects(regularItems, dummyArraySize);
+            } else {
+                createObjects(discountedItems, dummyArraySize);
+            }
+            int num;
+            int num2;
+            if(num2 == 0) {
+                switch (num) {
+                    case 0:
+                        return 1;
+                    case 1:
+                        return 2;
+                    case 2:
+                        return 3;
+                    case 3:
+                        return 1;
+                    case 4:
+                        return 2;
+                    case 5:
+                        return 3;
+                    case 6:
+                        return 1;
+                    case 7:
+                        return 2;
+                    case 8:
+                        return 3;
+                    case 9:
+                        return 1;
+                    case 10:
+                        return 2;
+                    case 11:
+                        return 3;
+                    case 12:
+                        return 1;
+                    case 13:
+                        return 2;
+                    case 14:
+                        return 3;
+                    default:
+                        return 0;
+                } else {
+                    for (int num = 0; num < 10000; num++) {
+                        num++;
+                        if (num%2==0){
+                            System.out.println("num is even")
+                        }
+                    }
+                }
+            }
+            System.out.println("Memory Consumed till now: " + loop + "::"+ regularItems + " "+discountedItems );
+            dummyArraySize *= dummyArraySize * 2;
         }
     }
 }
