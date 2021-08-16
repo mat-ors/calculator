0ece0142bc1b000ea9f8a55f49e7556454326acb
Matisz Orsolya <orsolya.matisz@frontendart.com>
2021-08-16 13:15:45 +0200

    Fixing file not found exception

--- new_branch/FileNotFoundException.java
+++ new_branch/FileNotFoundException.java
@@ -7,10 +7,4 @@ public class FilenotFound_Demo {
         File file = new File("E://file.txt");
         FileReader fr = new FileReader(file);
     }
-}public class Unchecked_Demo {
-
-    public static void main(String args[]) {
-        int num[] = {1, 2, 3, 4};
-        System.out.println(num[5]);
-    }
-} 
+}
