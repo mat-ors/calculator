00f2c72f2170b7c3dbc9b2bde5c856f4a60a00aa
Matisz Orsolya <orsolya.matisz@frontendart.com>
2021-08-16 13:08:19 +0200

    Added file not found exception

---
+++ new_branch/FileNotFoundException.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileReader;
+
+public class FilenotFound_Demo {
+
+    public static void main(String args[]) {
+        File file = new File("E://file.txt");
+        FileReader fr = new FileReader(file);
+    }
+}public class Unchecked_Demo {
+
+    public static void main(String args[]) {
+        int num[] = {1, 2, 3, 4};
+        System.out.println(num[5]);
+    }
+} 
